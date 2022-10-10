e5206879c7de2ffb56dd1655d21cac3f79734db3
Mark McNally <mark@macanalytics.ie>
2022-10-10 07:03:45 +0100

    Updated app css

--- src/Weather.js
+++ src/Weather.js
@@ -4,21 +4,33 @@ import "./Weather.css";
 export default function Weather () {
     return <div className="Weather">
         <form>
-            <input type="search" placeholder="Enter a City" className="form-control"/>
-        <input type="submit" value="Search" className="btn btn-primary"/>
+            <div className="row">
+                <div className="col-9">
+            <input type="search" placeholder="Enter a City" className="form-control" autoFocus= "on" />
+            </div>
+            <div className="col-3">
+        <input type="submit" value="Search" className="btn btn-primary w-100"/>
+        </div>
+        </div>
         </form>
-        <h1>Lisbon</h1>
+        <h1 className="mt-3">Lisbon</h1>
         <ul>
             <li>
                 Wednesday 07:00
             </li>
             <li>Mostly CLoudy</li>
         </ul>
-        <div className="row">
+        <div className="row mt-3">
             <div className="col-6">
-                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy" />
-                6°C
+                <div className= "clearfix">
+                <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="Mostly Cloudy" className="float-left"/>
+               <div className="float-left">
+                <span className= "temperature">6</span> <span className="unit">C</span>
             </div>
+            </div> 
+            </div>
+            
+           
             <div className="col-6">
                 <ul>
                     <li>Percipitation: 15%</li>
@@ -28,4 +40,5 @@ export default function Weather () {
             </div>
         </div>
         </div>;
+    
 }
